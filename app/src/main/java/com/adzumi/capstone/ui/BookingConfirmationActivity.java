610f6ea151d354d5f9443bf54ad34dfcc3869d1d
yomZsamora <samora.y@adzumi.co.ke>
2018-07-19 09:41:13 +0300

    Finalized forms design.

--- app/src/main/java/com/adzumi/capstone/ui/BookingConfirmationActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/BookingConfirmationActivity.java
@@ -1,15 +1,38 @@
 package com.adzumi.capstone.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.adzumi.capstone.R;
 
-public class BookingConfirmationActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BookingConfirmationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.finishButton)
+    Button mFinishButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_confirmation);
+        ButterKnife.bind(this);
+
+        mFinishButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFinishButton) {
+            Intent intent =  new Intent(BookingConfirmationActivity.this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/adzumi/capstone/ui/MapsActivity.java
+++ app/src/main/java/com/adzumi/capstone/ui/MapsActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.adzumi.capstone.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,10 +22,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     @BindView(R.id.my_toolbar) Toolbar myToolbar;
+    @BindView(R.id.confirmButton) Button mConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +42,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        mConfirmButton.setOnClickListener(this);
+
         myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), HairServicesActivity.class));
             }
         });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == mConfirmButton) {
+            Intent intent =  new Intent(MapsActivity.this, BookingConfirmationActivity.class);
+            startActivity(intent);
+        }
     }
 
     /**
